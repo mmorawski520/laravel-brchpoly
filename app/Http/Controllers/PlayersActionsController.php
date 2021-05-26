7aa1025df8706fa51815467708cb8ef10383310e
badylcookiestyle <73366240+badylcookiestyle@users.noreply.github.com>
2021-05-26 08:41:08 +0200

    Update PlayersActionsController.php

--- app/Http/Controllers/PlayersActionsController.php
+++ app/Http/Controllers/PlayersActionsController.php
@@ -13,10 +13,8 @@ class PlayersActionsController extends Controller
     public function index($id, $board_id,$task)
     {
          $players_in_board = Player::where("players.board_id", "=", $board_id)->get();
-     /*    $actions=PlayerActions::first()->tasks($id,$board_id,$task);
-         return view('players_actions.index', ["actions" => $actions, "players_in_board" => $players_in_board]);
-     */
-          if ($task == "current") {
+     
+        if($task == "current") {
             $boards = DB::select('select * from boards where user_id = ?', [$id, ]);
             $actions = Player::join("player_actions", "players.id", "=", "player_actions.player_id")->where("players.board_id", "=", $board_id)->where("player_actions.player_id", "=", $id)->orderBy("player_actions.created_at", "DESC")->paginate(5);
         }
@@ -31,13 +29,14 @@ class PlayersActionsController extends Controller
         $action=PlayerActions::where("id","=",$id)->first();
         $board=Player::where("id","=",$action->player_id)->first();
         $players_in_board = Player::where("players.board_id", "=",$board->board_id)->get();
-        return view('players_actions.edit',["action"=>$action,"players_in_board"=>$players_in_board,"actionTypes"=>$actionTypes,"id"=>$id]);    }
+        return view('players_actions.edit',["action"=>$action,"players_in_board"=>$players_in_board,"actionTypes"=>$actionTypes,"id"=>$id]);    
+    }
     public function updateAction(EditActionValidation $EditActionValidation,$id)
     {
-        //$id=$EditActionValidation->id;
-         $amount=$EditActionValidation->input("ReceiveValue");
+     
+        $amount=$EditActionValidation->input("ReceiveValue");
         $change=$EditActionValidation->input("actionSelect");
-     // return $id;
+ 
         return PlayerActions::first()->updateAction($EditActionValidation->id,$EditActionValidation);
     }
     public function destroy($id)
